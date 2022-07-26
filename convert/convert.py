a4f4fc4dddf60262b20d6c23969dbdbd43d67f9f
Looonk <frankyky97@gmail.com>
2022-07-25 23:52:08 -0400

    comillas

--- convert/convert.py
+++ convert/convert.py
@@ -3,7 +3,7 @@ import pandas as pd
 files = os.listdir()
 for f in files:
     if f.endswith(".xls") or f.endswith(".XLS"):
-        df = pd.read_excel(f, engine='xlrd')
-        writer = pd.ExcelWriter(f[:-4]+".xlsx", engine='xlsxwriter')
-        df.to_excel(writer, sheet_name='Sheet1')
+        df = pd.read_excel(f, engine="xlrd")
+        writer = pd.ExcelWriter(f[:-4]+".xlsx", engine="xlsxwriter")
+        df.to_excel(writer, sheet_name="Sheet1", index=False, header=False)
         writer.save()
--- main.py
+++ main.py
@@ -9,8 +9,8 @@ files = os.listdir("./in/"+folder[0])
 os.chdir("./in/"+folder[0]+"/")
 for f in files:
     if f.endswith(".xls") or f.endswith(".XLS"):
-        df = pd.read_excel(f, usecols="B,D", keep_default_na=False, engine='xlrd')
-        #df = pd.read_excel(f, usecols="A,G", keep_default_na=False, engine='xlrd')
+        df = pd.read_excel(f, usecols="B,D", keep_default_na=False, engine="xlrd")
+        #df = pd.read_excel(f, usecols="A,G", keep_default_na=False, engine="xlrd")
         for i in df.itertuples():
             if not i.__contains__(""):
                 if i.__getattribute__("_2").__contains__("\""):
@@ -30,13 +30,13 @@ for f in files:
                 img.save("../../output/" + folder[0] + "/" + str(f)[:-4] + "/" + name)
 
                 img = Image.open("../../output/" + folder[0] + "/" + str(f)[:-4] + "/" + name)
-                rgb_img = img.convert('RGB')
+                rgb_img = img.convert("RGB")
                 rgb_img.save("../../output/" + folder[0] + "/" + str(f)[:-4] + "/" + name[:-4] + " not.png")
 
-                canvas = Image.new('RGB', (290, 320), 'white')
+                canvas = Image.new("RGB", (290, 320), "white")
                 img_draw = ImageDraw.Draw(canvas)
                 font = ImageFont.truetype("../../times.ttf", size=19)
-                img_draw.text((110, 300), str(i.__getattribute__("_1")), fill='black', font=font)
+                img_draw.text((110, 300), str(i.__getattribute__("_1")), fill="black", font=font)
                 canvas.save("../../output/" + folder[0] + "/" + str(f)[:-4] + "/" + name[:-4] + " bg.png")
 
                 image = Image.open("../../output/" + folder[0] + "/" + str(f)[:-4] + "/" + name[:-4] + " not.png")
@@ -52,4 +52,4 @@ for f in files:
 
         os.remove("../../output/" + folder[0] + "/" + str(f)[:-4] + "/Código - Descripción.png")
 os.chdir("../../")
-shutil.make_archive("./output/" + folder[0], 'zip', "./output/" + folder[0])
+shutil.make_archive("./output/" + folder[0], "zip", "./output/" + folder[0])
